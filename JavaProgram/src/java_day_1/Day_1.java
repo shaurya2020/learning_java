74ac542c26cf4348e349153a4ff2150d6bd963e5
Ayushgiggit <ayushmaurya9450@gmail.com>
2026-09-20 10:35:03 +0530

    day1

---
+++ JavaProgram/src/java_day_1/Day_1.java
@@ -0,0 +1,17 @@
+package java_day_1;
+
+public class Day_1 {
+//	swapping 2 number
+	public static void main(String[] args) {
+	int m = 9, n =5;
+		
+		System.out.println("Before swapping : m = "+m+", n = "+n);
+		
+		int temp = m;
+		m=n;
+		n=temp;
+		
+		System.out.println("After swapping : m = "+m+", n = "+n);
+		
+	}
+}
